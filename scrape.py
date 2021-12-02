51c597595bc71ac8ff6e235bc8ca87a1a7a82fb8
Henry Hamer <henryhamer@oui.local>
2021-12-02 23:58:01 +1000

    quantified wind info

--- scrape.py
+++ scrape.py
@@ -2,6 +2,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+# regex
+import re
+
 # urls for each of the locations
 urls = {'new brighton' : 'https://magicseaweed.com/New-Brighton-Beach-Surf-Report/5212/',
         'yaroomba' : 'https://magicseaweed.com/Yaroomba-Surf-Report/5362/',
@@ -27,7 +30,18 @@ def get_wind(location):
     divs  = soup.findAll('div')
     for div in divs:
         if div.has_attr("data-original-title"):
-                return div["data-original-title"].strip()
+            if div["data-component"] == "WIND":
+                wind_string = div["data-original-title"].strip()
+                break
+    return Wind(wind_string)
+
+class Wind:
+    def __init__(self, wind_string):
+        m = re.match(r"(?P<speed>.*)mph - (?P<strength>.*), (?P<direction>.*) - (?P<angle>.*)°", wind_string)
+        self.speed = int(m.group('speed')) * 1.6
+        self.strength = m.group('strength')
+        self.direction = m.group('direction')
+        self.angle = int(m.group('angle'))
 
 # conditions class - will eventually have all relevant info on surf conditions
 class Conditions:
@@ -43,4 +57,4 @@ location = input("enter location: ")
 if location in urls:
     data = Conditions(location)
     print(f"swell: {data.swell}")
-    print(f"wind: {data.wind}")
+    print(f"wind: {data.wind.speed}km/h, {data.wind.angle}°")
