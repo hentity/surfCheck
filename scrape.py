6dee6a7ca994cd61c4815d3c3fadc1e542497823
Henry Hamer <henryhamer@oui.local>
2021-12-02 23:06:41 +1000

    show swell and wind for input location

--- scrape.py
+++ scrape.py
@@ -1,15 +1,38 @@
 import requests
-import urllib.request
-import time
 from bs4 import BeautifulSoup
 
-url = 'https://magicseaweed.com/New-Brighton-Beach-Surf-Report/5212/'
-response = requests.get(url)
+urls = {'new brighton' : 'https://magicseaweed.com/New-Brighton-Beach-Surf-Report/5212/',
+        'yaroomba' : 'https://magicseaweed.com/Yaroomba-Surf-Report/5362/',
+        'south straddie' : 'https://magicseaweed.com/South-Stradbroke-Island-Surf-Report/1010/',
+        'palm beach' : 'https://magicseaweed.com/Palm-Beach-Surf-Report/6140/',
+        'noosa' : 'https://magicseaweed.com/Tea-Tree-Noosa-Surf-Report/544/'}
 
-soup = BeautifulSoup(response.text, "html.parser")
+def get_swell(location):
+    url = urls[location]
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    lis  = soup.findAll('li')
+    for li in lis:
+        if li.has_attr( "class" ) and li["class"] == ['rating-text', 'text-dark']:
+                return li.text.strip()
 
-lis  = soup.findAll('li')
+def get_wind(location):
+    url = urls[location]
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    divs  = soup.findAll('div')
+    for div in divs:
+        if div.has_attr("data-original-title"):
+                return div["data-original-title"].strip()
 
-for li in lis:
-    if li.has_attr( "class" ) and li["class"] == ['rating-text', 'text-dark']:
-            print(li.text.strip())
+class Conditions:
+    def __init__(self, location):
+        self.location = location
+        self.swell = get_swell(location)
+        self.wind = get_wind(location)
+
+location = input("enter location: ")
+if location in urls:
+    data = Conditions(location)
+    print(f"swell: {data.swell}")
+    print(f"wind: {data.wind}")
